51e27893abff1398e56b275504ef8b379bd7022d
olbapDev <pa.garridoc@alumnos.duoc.cl>
2021-02-25 18:42:48 -0300

    se modifica el home y se crea app de contacto inicial sin conexion

--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from proyect.models import Project
 # Create your views here.
 def HomeView(request):
-    return render(request,'home.html')
+    projects= Project.objects.all()
+    data={
+        'proyectos': projects
+    }
+    return render(request,'home.html',data)
